484bc6c0598e4ffaeaf61a2ba91e772d476fc25c
Thomas Jankowski <thomas.jankowski@gestimum.com>
2024-02-21 16:00:17 +0100

    update: testConnexions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,13 +5,17 @@ import { GrStatusWarning, GrStatusGood } from "react-icons/gr";
 
 const Home = () => {
 
-  const [status, setStatus] = useState(false)
+  const [apiStatus, setApiStatus] = useState(false)
+  const [apiErpStatus, setApiErpStatus] = useState(false)
+  const [kazeCredentialsStatus, setKazeCredentialsStatus] = useState(false)
   const [apiStatusLoading, setApiStatusLoading] = useState(false)
   const [connexionKazeERPLoading, setConnexionKazeERPLoading] = useState(false)
+  const [kazeCredentialsLoading, setKazeCredentialsLoading] = useState(false)
 
   useEffect(() => {
     testAPIStatus()
     testConnexionKazeERP()
+    testKazeCredentials()
   }, [])
 
   const testAPIStatus = async () => {
@@ -22,11 +26,11 @@ const Home = () => {
       console.log(process.env.KAZE_API_URL)
       const response = await fetch(process.env.KAZE_API_URL || 'http://localhost:3000/')
       const data = await response.json()
-      if(data.status === 'ok') setStatus(true)
-      else setStatus(false)
+      if(data.status === 'ok') setApiStatus(true)
+      else setApiStatus(false)
     } catch (error) {
       console.error('Error:', error)
-      setStatus(false)
+      setApiStatus(false)
     }
     setApiStatusLoading(false)
   }
@@ -37,14 +41,30 @@ const Home = () => {
     try {
       const response = await fetch(`${process.env.KAZE_API_URL}/connexionERP`)
       const data = await response.json()
-      if(data.status === 'ok') setStatus(true)
-      else setStatus(false)
+      if(data.status === 'ok') setApiErpStatus(true)
+      else setApiErpStatus(false)
     } catch (error) {
       console.error('Error:', error)
-      setStatus(false)
+      setApiErpStatus(false)
     }
     setConnexionKazeERPLoading(false)
   }
+
+  const testKazeCredentials = async () => {
+    setKazeCredentialsLoading(true)
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    try {
+      const response = await fetch(`${process.env.KAZE_API_URL}/v1/kaze/testConnection`)
+      const data = await response.json()
+      if(data.status === 'ok') setKazeCredentialsStatus(true)
+      else setKazeCredentialsStatus(false)
+    } catch (error) {
+      console.error('Error:', error)
+      setKazeCredentialsStatus(false)
+    }
+    setKazeCredentialsLoading(false)
+  }
+
 
 
   return (
@@ -52,24 +72,27 @@ const Home = () => {
       <Header title="Accueil" subtitle="Console d'administration ERP - KAZE" />
       <div className=' mt-8 w-full bg-blue-100 h-96 rounded-md shadow-xl p-6 flex flex-col gap-8'>
         <div className='flex items-center'>
-        <button onClick={testAPIStatus} className='mr-4 bg-primary-400 text-white px-4 py-2 rounded-md hover:bg-primary-100'>Tester</button>
-         <h2 className='font-bold'>Connexion à l'API :</h2>
+          <button onClick={testAPIStatus} className='mr-4 bg-primary-400 text-white px-4 py-2 rounded-md hover:bg-primary-100'>Tester</button>
+          <h2 className='font-bold'>Connexion à l'API :</h2>
           {apiStatusLoading && <div className='ml-4 font-semibold italic'> Chargement... </div>}
-          {status && !apiStatusLoading && <GrStatusGood size={26} className='ml-4 text-green-500' />}
-          {!status && !apiStatusLoading && <GrStatusWarning size={26} className='ml-4 text-red-500' />}
-        
-        {/* button to test connextion  */}
+          {apiStatus && !apiStatusLoading && <GrStatusGood size={26} className='ml-4 text-green-500' />}
+          {!apiStatus && !apiStatusLoading && <GrStatusWarning size={26} className='ml-4 text-red-500' />}
         </div>
 
-        {/* si connexion ERP est établie, afficher le status de la connexion entre Kaze et l'ERP */}
         <div className='flex items-center'>
-        <button onClick={testConnexionKazeERP} className='mr-4 bg-primary-400 text-white px-4 py-2 rounded-md hover:bg-primary-100'>Tester</button>
-         <h2 className='font-bold'>Connexion entre KAZE et l'ERP : </h2>
+          <button onClick={testConnexionKazeERP} className='mr-4 bg-primary-400 text-white px-4 py-2 rounded-md hover:bg-primary-100'>Tester</button>
+          <h2 className='font-bold'>Connexion à l'ERP : </h2>
           {connexionKazeERPLoading && <div className='ml-4 font-semibold italic'> Chargement... </div>}
-          {status && !connexionKazeERPLoading && <GrStatusGood size={26} className='ml-4 text-green-500' />}
-          {!status && !connexionKazeERPLoading && <GrStatusWarning size={26} className='ml-4 text-red-500' />}
-        
-        {/* button to test connextion  */}
+          {apiErpStatus && !connexionKazeERPLoading && <GrStatusGood size={26} className='ml-4 text-green-500' />}
+          {!apiErpStatus && !connexionKazeERPLoading && <GrStatusWarning size={26} className='ml-4 text-red-500' />}
+        </div>
+
+        <div className='flex items-center'>
+          <button onClick={testKazeCredentials} className='mr-4 bg-primary-400 text-white px-4 py-2 rounded-md hover:bg-primary-100'>Tester</button>
+          <h2 className='font-bold'>Connexion à Kaze : </h2>
+          {kazeCredentialsLoading && <div className='ml-4 font-semibold italic'> Chargement... </div>}
+          {kazeCredentialsStatus && !kazeCredentialsLoading && <GrStatusGood size={26} className='ml-4 text-green-500' />}
+          {!kazeCredentialsStatus && !kazeCredentialsLoading && <GrStatusWarning size={26} className='ml-4 text-red-500' />}
         </div>
       </div>
       
