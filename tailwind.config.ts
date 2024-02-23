ce78b06cef3488d339c3b449ed0404e2e10b69dc
ThomasJanko <thomas78125@gmail.com>
2024-02-23 11:00:27 +0100

    shadcn + MobilNav

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,11 +26,6 @@ const config = {
           400: "#f47922",
         }
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
